6193ef14b0d1c1c38de8e9265cfac51439dba1ac
Alex Naidis <alex.naidis@linux.com>
2017-04-09 01:29:27 +0200

    init: Weaken property override security for the init  extension
    Sometimes we need to override ro.* properties by using
    our vendor init extension.
    
    Previously there was a security check which was blocking
    that.
    To resolve the issue, we need to weaken the security check
    during the execution of our vendor init extension.
    
    This is safe because the vendor init extension gets
    executed as part of init construction and it is
    considered a trusted system component.

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -101,6 +101,8 @@ struct PropertyAuditData {
     const char* name;
 };
 
+static bool weaken_prop_override_security = false;
+
 void property_init() {
     mkdir("/dev/__properties__", S_IRWXU | S_IXGRP | S_IXOTH);
     CreateSerializedPropertyInfo();
@@ -164,8 +166,8 @@ static uint32_t PropertySet(const std::string& name, const std::string& value, s
 
     prop_info* pi = (prop_info*) __system_property_find(name.c_str());
     if (pi != nullptr) {
-        // ro.* properties are actually "write-once".
-        if (StartsWith(name, "ro.")) {
+        // ro.* properties are actually "write-once", unless the system decides to
+        if (StartsWith(name, "ro.") && !weaken_prop_override_security) {
             *error = "Read-only property was already set";
             return PROP_ERROR_READ_ONLY_PROPERTY;
         }
@@ -913,8 +915,14 @@ void property_load_boot_defaults(bool load_debug_prop) {
         }
     }
 
+    // Weaken property override security during execution of the vendor init extension
+    weaken_prop_override_security = true;
+
     // Update with vendor-specific property runtime overrides
     vendor_load_properties();
+
+    // Restore the normal property override security after init extension is executed
+    weaken_prop_override_security = false;
 
     property_initialize_ro_product_props();
     property_derive_build_fingerprint();
